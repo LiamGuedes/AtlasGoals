503cfa941a64425d558fd3a6641357723332ebae
Willian Guedes <willianguedes@Willians-MacBook-Air.local>
2024-11-18 17:45:18 -0300

    ci: fix null error in coverage.info

--- Scripts/generate_lcov.sh
+++ Scripts/generate_lcov.sh
@@ -8,15 +8,8 @@ XCRESULT_PATH="$DERIVED_DATA_PATH/Logs/Test/*.xcresult"
 LCOV_OUTPUT="coverage.info"
 TEMP_DIR=$(mktemp -d)
 
-ls
-
-if ! command -v xcrun &> /dev/null; then
-  echo "xcrun could not be found. Please ensure Xcode Command Line Tools are installed."
-  exit 1
-fi
-
-if ! command -v lcov &> /dev/null; then
-  echo "lcov could not be found. Please install it using Homebrew: brew install lcov"
+if ! command -v xcrun &> /dev/null || ! command -v jq &> /dev/null; then
+  echo "xcrun or jq not found. Please install Xcode Command Line Tools and jq."
   exit 1
 fi
 
@@ -25,18 +18,20 @@ if [ ! -e $XCRESULT_PATH ]; then
   exit 1
 fi
 
-echo "Extracting code coverage data from $XCRESULT_PATH..."
+echo "Extracting coverage data from $XCRESULT_PATH..."
 
 COVERAGE_JSON="$TEMP_DIR/coverage.json"
 xcrun xccov view --report --json $XCRESULT_PATH > $COVERAGE_JSON
 
 echo "Converting JSON coverage to LCOV format..."
 
-echo "Creating $LCOV_OUTPUT..."
-
 echo "TN:" > $LCOV_OUTPUT
 
-jq -r '.targets[] | select(.name != "System" and .name != "TestBundle") | .files[] | "\(.path)\nDA:\(.lineNumber),\(.executionCount)"' $COVERAGE_JSON | while read -r line; do
+jq -r '.targets[] 
+        | select(.name != "System" and .name != "TestBundle") 
+        | .files[] 
+        | select(.lineNumber != null and .executionCount != null) 
+        | "\(.path)\nDA:\(.lineNumber),\(.executionCount)"' $COVERAGE_JSON | while read -r line; do
   if [[ $line == /* ]]; then
     echo "SF:$line" >> $LCOV_OUTPUT
   elif [[ $line == DA:* ]]; then
@@ -45,6 +40,8 @@ jq -r '.targets[] | select(.name != "System" and .name != "TestBundle") | .files
     echo "end_of_record" >> $LCOV_OUTPUT
   fi
 done
+
+echo "end_of_record" >> $LCOV_OUTPUT
 
 rm -rf $TEMP_DIR
 
